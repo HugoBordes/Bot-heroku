ae01e40b95d8fe9396bd23e2c5f4a7d9adba4e50
Commantary <gmail@gmail.com>
2017-08-26 03:55:53 +0200

    Updated NODE & NPM

--- module/ban.js
+++ module/ban.js
@@ -4,35 +4,30 @@
  * 
  */
 
-module.exports = function(client){
-    
-    client.on('message', message =>{
-        var h = message.author.lastMessage.createdAt;
-        var nom = message.author.username;
+module.exports = function (client) {
+  client.on('message', message => {
+    var h = message.author.lastMessage.createdAt
+    var nom = message.author.username
 
-        if(message.content.startsWith('+ban')) {
-            // Pour avoir la mention du gars
-            var member = message.mentions.members.first();
-            let modRole = message.guild.roles.find("name", "Mod");
+    if (message.content.startsWith('+ban')) {
+      // Pour avoir la mention du gars
+      var member = message.mentions.members.first()
+      let modRole = message.guild.roles.find('name', 'Mod')
 
-            //deuxieme If
-            if(message.member.roles.has(modRole.id)) {
-
-                //ban
-                member.ban(member)
-                .then((member) => {
-                    // Message réussis
-                    message.channel.send(":wave: " + member.displayName + " à bien été banni :wave: ");
-                    console.log(h+' +ban mis par: '+nom);
-                })
-            } else {
-                // Message de fail
-                message.channel.send('Acces refusé');
-                console.log(h + ' Tentative de ban par: ' + nom);
-            }
-        }
-    })
-
-
-            }
-        
+      // deuxieme If
+      if (message.member.roles.has(modRole.id)) {
+        // ban
+        member.ban(member)
+          .then((member) => {
+            // Message réussis
+            message.channel.send(':wave: ' + member.displayName + ' à bien été banni :wave: ')
+            console.log(h + ' +ban mis par: ' + nom)
+          })
+      } else {
+        // Message de fail
+        message.channel.send('Acces refusé')
+        console.log(h + ' Tentative de ban par: ' + nom)
+      }
+    }
+  })
+}
--- module/kick.js
+++ module/kick.js
@@ -4,28 +4,27 @@
  * 
  */
 
-module.exports = function(client){
+module.exports = function (client) {
+  client.on('message', message => {
+    // Mise en place des variables
+    var h = message.author.lastMessage.createdAt
+    var nom = message.author.username
 
-        client.on('message', message =>{
-            // Mise en place des variables
-            var h = message.author.lastMessage.createdAt;
-            var nom = message.author.username;
-
-            if(message.content.startsWith('+kick')) {
-                // Pour avoir la mention du membre
-                var member = message.mentions.members.first();
-                
-                if (message.member.roles.has(modRole.id)) {
-                    member.kick().then((member) => {
-                        // Message réussis
-                        message.channel.send(":wave: " + member.displayName + " à bien été kick :point_right: ");
-                        console.log(h+' +kick mis par: '+nom);
-                    }).catch(() => {
-                        // Message du fail
-                        message.channel.send("Acces refusé");
-                        console.log(h+' Tentative de "+kick" de: '+nom);
-                    })
-                }
-            }
-        })   
+    if (message.content.startsWith('+kick')) {
+      // Pour avoir la mention du membre
+      var member = message.mentions.members.first()
+      let modRole = message.guild.roles.find('name', 'Mod')
+      if (message.member.roles.has(modRole.id)) {
+        member.kick().then((member) => {
+          // Message réussis
+          message.channel.send(':wave: ' + member.displayName + ' à bien été kick :point_right: ')
+          console.log(h + ' +kick mis par: ' + nom)
+        }).catch(() => {
+          // Message du fail
+          message.channel.send('Acces refusé')
+          console.log(h + ' Tentative de "+kick" de: ' + nom)
+        })
+      }
+    }
+  })
 }
